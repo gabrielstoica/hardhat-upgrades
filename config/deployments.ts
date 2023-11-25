efbae8f9da527cd7482f75f407ffb120e0983b51
gabrielstoica <marius_gabriel1998@yahoo.com>
2023-11-25 17:04:50 +0200

    feat(deployments): automatically store the proxy contract address once deployed

--- config/deployments.ts
+++ config/deployments.ts
@@ -1,7 +1,9 @@
+// Deployments will be automatically updated once a new proxy 
+// contract is deployed on one of these chains
 export const deployments: Record<string, string> = {
   mainnet: "YOUR-MAINNET-DEPLOYMENT-ADDRESS",
   polygon: "YOUR-POLYGON-DEPLOYMENT-ADDRESS",
   sepolia: "YOUR-SEPOLIA-DEPLOYMENT-ADDRESS",
   goerli: "YOUR-GOERLI-DEPLOYMENT-ADDRESS",
-  mumbai: "0xD4aB7301ECe7AAe4F08065f1aB27939AB7f99d26",
+  mumbai: "YOUR-MUMBAI-DEPLOYMENT-ADDRESS",
 };
--- utils.ts
+++ utils.ts
@@ -1,4 +1,6 @@
+import { writeFileSync } from "fs";
 import { run } from "hardhat";
+import { deployments } from "./config/deployments";
 
 export const verify = async (contractAddress: string) => {
   console.log("Verifying contract...");
@@ -12,3 +14,16 @@ export const verify = async (contractAddress: string) => {
 };
 
 export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export const addDeployment = (network: string, proxyAddress: string) => {
+  deployments[network] = proxyAddress;
+
+  // Convert the updated deployments object back to a string
+  const deploymentsContent = `export const deployments: Record<string, string> = ${JSON.stringify(
+    deployments,
+    null,
+    2
+  )};`;
+
+  writeFileSync("./config/deployments.ts", deploymentsContent, "utf-8");
+};
