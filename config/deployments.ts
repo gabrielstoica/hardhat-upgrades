55706091ac80bce5e4b946963f935ee5b42e8609
gabrielstoica <marius_gabriel1998@yahoo.com>
2023-11-26 20:39:00 +0200

    docs(deployments): improve documentation

--- config/deployments.ts
+++ config/deployments.ts
@@ -1,5 +1,5 @@
 // Deployments will be automatically updated once a new proxy 
-// contract is deployed on one of these chains
+// contract is deployed on one of the networksConfig (config/index.ts) chains
 export const deployments: Record<string, string> = {
   mainnet: "YOUR-MAINNET-DEPLOYMENT-ADDRESS",
   polygon: "YOUR-POLYGON-DEPLOYMENT-ADDRESS",
