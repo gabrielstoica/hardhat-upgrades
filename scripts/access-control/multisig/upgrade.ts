4741a2a7898f6d162a729d9e9d5d8614c3e6d563
gabrielstoica <marius_gabriel1998@yahoo.com>
2023-11-26 20:38:41 +0200

    feat(multisigs): add multisig support and update upgrade script

--- scripts/access-control/multisig/upgrade.ts
+++ scripts/access-control/multisig/upgrade.ts
@@ -3,6 +3,7 @@ import { MyTokenV2__factory } from "typechain-types";
 import { verify } from "utils";
 import { networksConfig } from "config";
 import { deployments } from "config/deployments";
+import { multisigs } from "config/multisigs";
 
 async function main() {
   const { network } = hardhatArguments;
@@ -10,17 +11,18 @@ async function main() {
     throw new Error("Please specify the target network. Aborting...");
   }
 
-  const multisigAddress = "";
+  // Get the UPGRADER_ROLE multisig contract address
+  const multisigAddress = multisigs[network].UPGRADER_ROLE;
 
-  // Create a factory for the new implementation
+  // Create the factory instance for the new implementation
   const factory: MyTokenV2__factory = <MyTokenV2__factory>await ethers.getContractFactory("MyTokenV2");
 
+  // Create the upgrade proposal through the defender admin client 
   const proposal = await defender.proposeUpgrade(deployments[network!], factory, {
     title: "Title of the proposal",
     description: "Description of the proposal",
     multisig: multisigAddress,
   });
-
   console.log("Upgrade proposal created at: ", proposal.url);
 
   // Retrieve the new implementation address within the proposal's metadata
